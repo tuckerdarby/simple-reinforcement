a8106321e78ca1d0a85733e21b8f13f4a899b493
tuck <rdarby@mymail.mines.edu>
2016-08-30 19:31:15 -0600

    Added data visuals

--- smartcab/agent2.py
+++ smartcab/agent2.py
@@ -15,95 +15,132 @@ class LearningAgent(Agent):
         # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
-        # TODO: Initializ
-        # e any additional variables here
-        self.learner = 0.5
-        self.discount = 0.5
-        self.trips = []
+        # TODO: Initialize
+        #parameters
+        self.learner = 0.9
+        self.discount = 0.7
+        self.q = 0
+        #state/action
         self.action = None
         self.actions = [None, 'forward', 'left', 'right']  # really just the keys to rewards
         self.rewards = {}
         self.init_states()
-        self.q = 0
+        self.runs = 0
         # Measurements
+        self.trips = []
+        # - must reset
+        self.errors = []
+        self.moves = 0
+        self.acceptable = 0
         self.total_reward = 0
-        self.actions_taken = 0
 
     def init_states(self):
         # Inputs: light, oncoming, right, left, direction
         lights = ['green', 'red']
+        #traffic = [True, False]
         oncomings = ['forward', 'left', 'right', None]
-        rights = ['forward', 'left', 'right', None]
-        lefts = ['forward', 'left', 'right', None]
+        cross = [True, False]
+        #rights = ['forward', 'left', 'right', None]
+        #lefts = ['forward', 'left', 'right', None]
         directions = ['forward', 'left', 'right']
-        states = list(product(lights, oncomings, rights, lefts, directions))
+        states = list(product(lights, oncomings, cross, directions))
         # reward tables
         for action in self.actions:
             self.rewards[action] = {}
             for state in states:
-                self.rewards[action][state] = random.uniform(0, 0.2)
+                self.rewards[action][state] = random.uniform(0, 10)
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
-        self.trips.append({'reward': self.total_reward, 'actions': self.actions_taken})
+        self.log_trial()
         self.total_reward = 0
-        self.actions_taken = 0
+        self.acceptable = 0
+        self.moves = 0
+        self.errors = []
 
     def update(self, t):
         self.define_state()
-        oldState = self.state
+        old_state = self.state
         # choose action
         action, estimate = self.best_action()
         reward = self.env.act(self, action)
         # Get next state
         self.define_state()
         utility = reward + self.discount * estimate
-        self.rewards[action][oldState] += self.learner * (utility - self.q)
+        self.rewards[action][old_state] += self.learner * (utility - self.q)
         self.q = self.rewards[action][self.state]
-        self.total_reward += reward
-        # TODO: Learn policy based on state, action, reward
-        #print("LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}"
-         #     .format(deadline, inputs, action, reward))  # [debug]
+        self.log_move(reward)
 
     def best_action(self):
         chosen = None
         estimate = -100
+        #greedy choice
         for action in self.actions:
-            if self.rewards[action][self.state] > estimate:
+            if self.rewards[action][self.state] >= estimate:
                 chosen = action
                 estimate = self.rewards[action][self.state]
+
+        #randomize choice
+        randoming = False
+        if randoming:
+            rand = random.uniform(0, 1)
+            if rand > (min(0.8, self.runs) * 0.1):
+                random_act = random.randint(0, len(self.actions)-1)
+                chosen = self.actions[random_act]
+                estimate = self.rewards[chosen][self.state]
+
         return chosen, estimate
 
     def define_state(self):
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
-        self.state = (inputs['light'], inputs['oncoming'], inputs['right'], inputs['left'], self.next_waypoint)
+        #traffic = (inputs['oncoming'] != None or inputs['right'] != None or inputs['left'] != None)
+        cross = (inputs['right'] != None or inputs['left'] != None)
+        self.state = (inputs['light'], inputs['oncoming'], cross, self.next_waypoint)
+        #self.state = (inputs['light'], traffic, self.next_waypoint)
+        #self.state = (inputs['light'], inputs['oncoming'], inputs['right'], inputs['left'], self.next_waypoint)
+
+    def log_move(self, reward):
+        if reward < 0:
+            self.errors.append(reward)
+        else:
+            self.acceptable += 1
+        self.total_reward += reward
+        self.moves += 1
+
+    def log_trial(self):
+        trip = {
+            'reward': self.total_reward,
+            'moves': self.moves,
+            'errors': sum(self.errors),
+            'acceptable': self.acceptable
+        }
+        self.runs += 1
+        self.trips.append(trip)
 
 
 def run():
     """Run the agent for a finite number of trials."""
-
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
     e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
-    # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
+    sim = Simulator(e, update_delay=0.0, display=False)
+    sim.run(n_trials=1000)  # run for a specified number of trials
 
-    # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
-    # NOTE: To speed up simulation, reduce update_delay and/or set display=False
+    #data crunch
+    visualize_data(['reward', 'moves', 'errors', 'acceptable'], a.trips)
 
-    sim.run(n_trials=100)  # run for a specified number of trials
-    # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
-    trips = a.trips
-    totals = map(lambda x: x['reward'], trips)
-    moves = map(lambda x: x['actions'], trips)
-    fixed = map(lambda x: x['reward']/float(max(1, x['actions'])), trips)
-    print sum(totals) / len(totals)
-    #sns.distplot(x='actions', y='reward', data=trips)
-    #sns.regplot(x=totals, y=moves)
-    sns.barplot(x=range(len(totals)), y=totals)
+
+def visualize_data(names, data):
+    fig = sns.plt.figure()
+    num = len(names)
+    for i in range(num):
+        ax = fig.add_subplot(num, 1, 1 + i)
+        ax.set_title(names[i])
+        values = map(lambda x: x[names[i]], data)
+        ax.plot(values)
     sns.plt.show()
 
 
